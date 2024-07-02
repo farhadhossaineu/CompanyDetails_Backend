ee8a1cada136a135ca78b5690ec5a82b89bafdbe
farhad <farhad121450@gmail.com>
2024-07-02 13:55:01 +0600

    Comapany deleted

--- controllers/company.controllers.ts
+++ controllers/company.controllers.ts
@@ -2,6 +2,7 @@ import { NextFunction, Request, Response } from "express";
 import { CatchAsyncError } from "../middleware/catchAsyncErrors";
 import companyModel from "../models/company.model";
 import ErrorHandler from "../utils/ErrorHandler";
+const mongoose = require("mongoose");
 
 export interface IRegister {
   name: string;
@@ -49,6 +50,88 @@ export const getCompanyNames = CatchAsyncError(
       res.status(200).json({ success: true, companies });
     } catch (error: any) {
       next(new ErrorHandler(error.message, 404));
+    }
+  }
+);
+
+export interface ICompanyData {
+  _id: string;
+  name: string;
+  address: string;
+  numberOfEmp: number;
+}
+
+export const editCompanyNames = CatchAsyncError(
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const companyData = req.body as ICompanyData;
+      const companyDoc = await companyModel.findOne({ _id: companyData._id });
+      if (companyDoc === null) {
+        return res
+          .status(404)
+          .json({ success: false, message: "company not found" });
+      } else {
+        companyDoc.name = companyData.name;
+        companyDoc.address = companyData.address;
+        companyDoc.numberOfEmp = companyData.numberOfEmp;
+
+        await companyDoc.save();
+      }
+      return res
+        .status(200)
+        .json({ success: true, message: "Updated successfully" });
+    } catch (error: any) {
+      next(new ErrorHandler(error.message, 404));
+    }
+  }
+);
+
+export interface ICompanyDelete {
+  _id: string;
+}
+
+export const deleteCompany = CatchAsyncError(
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const { _id } = req.body as ICompanyDelete;
+
+      if (!mongoose.Types.ObjectId.isValid(_id)) {
+        return res
+          .status(400)
+          .json({ success: false, message: "Invalid company ID" });
+      }
+
+      const companyDoc = await companyModel.findOneAndDelete({ _id });
+
+      if (!companyDoc) {
+        return res
+          .status(404)
+          .json({ success: false, message: "Company not found" });
+      }
+
+      return res
+        .status(200)
+        .json({ success: true, message: "Deleted successfully" });
+    } catch (error: any) {
+      next(new ErrorHandler(error.message, 500));
+    }
+  }
+);
+
+export const EmployeeMoreThan10 = CatchAsyncError(
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const allCompanies = await companyModel.find({});
+
+      const filteredCompanies = allCompanies.filter(
+        (company) => company.numberOfEmp > 10
+      );
+
+      return res
+        .status(200)
+        .json({ success: true, companies: filteredCompanies });
+    } catch (error: any) {
+      next(new ErrorHandler(error.message, 500));
     }
   }
 );
--- routes/company.route.ts
+++ routes/company.route.ts
@@ -1,6 +1,9 @@
 import express from "express";
 import {
+  EmployeeMoreThan10,
   createCompany,
+  deleteCompany,
+  editCompanyNames,
   getCompany,
   getCompanyNames,
 } from "../controllers/company.controllers";
@@ -12,5 +15,8 @@ companyRouter.post("/user", createUser);
 companyRouter.post("/register", createCompany);
 companyRouter.get("/getsComp", getCompany);
 companyRouter.get("/getCompanyNames", getCompanyNames);
+companyRouter.put("/editCompanyNames", editCompanyNames);
+companyRouter.delete("/deleteCompany", deleteCompany);
+companyRouter.get("/Employee", EmployeeMoreThan10);
 
 export default companyRouter;
